a4707712cca3b5a067d1cf55cf28d1e3ce514c9c
Smisiw <70029516+Smisiw@users.noreply.github.com>
2025-06-23 13:58:03 +0700

    Update CrptApi.java

--- CrptApi.java
+++ CrptApi.java
@@ -1,7 +1,5 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.io.IOException;
 import java.net.URI;
